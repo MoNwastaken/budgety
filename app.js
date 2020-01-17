0744066e81f0cd8719e8de06893f10cb80bf78aa
Lanczi Dominik <nhcmon@gmail.com>
2020-01-17 15:28:32 +0100

    94

--- app.js
+++ app.js
@@ -229,6 +229,16 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     };
 
+    var updatePercentages = function () {
+
+        // 1. Calculate percentage
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+
+    };
+
     var ctrlAddItem  = function() {
         var input, newItem;
 
@@ -248,6 +258,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
 
     };
@@ -269,6 +282,8 @@ var controller = (function (budgetCtrl, UICtrl) {
             UICtrl.deleteListItem(itemID);
             // 3. Update and show the new budget
             updateBudget();
+            // 4. Calculate and update percentages
+            updatePercentages();
         }
 
     };
