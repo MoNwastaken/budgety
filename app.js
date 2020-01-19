a2df3b8ba5daa27a562b1393b3b62362fd1050fb
MoNwastaken <nhcmon@gmail.com>
2020-01-19 10:07:46 +0100

    98

--- app.js
+++ app.js
@@ -147,7 +147,8 @@ var UIController = (function () {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function (num, type) {
@@ -262,6 +263,16 @@ var UIController = (function () {
             });
         },
 
+        displayMonth: function() {
+            var now, months, month, year;
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
         getDOMstrings: function() {
             return DOMStrings
         }
@@ -363,6 +374,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('App has started');
+            UICtrl.displayMonth();
             // Reset data !! Make a button for it later !!
             UICtrl.displayBudget({
                 budget: 0,
